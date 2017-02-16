2f6252beb2599002b17ee3a38a7ba0afcaf9be5e
sabruss <sabruss@users.noreply.github.com>
2017-02-16 06:10:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,22 @@ const Discord = require("discord.js");
 // config file
 const config = require("./config.json");
 
+const level_alias = {
+    0: "Membre",
+    1: "Modérateur",
+    2: "Administrateur",
+    3: "Bot Owner"
+}
+
+
 // autoReconnect is enabled
 var bot = new Discord.Client({autoReconnect: true});
 
 // Ready? Set? Go!
 bot.on('ready', () => {
     bot.user.setStatus("Online"); //dnd , online , ldle
-    bot.user.setGame("" + config.prefix + "help");
-    console.log("Bot Ok");
+    bot.user.setGame("Bot Discord, use " + config.prefix + "help");
+    console.log("Bot connecté");
 });
 
 bot.on("disconnect", () => {
@@ -29,31 +37,40 @@ bot.on("error", err => {
     console.log(err)
 })
 
+process.on("unhandledRejection", err => {
+    console.error("Uncaught Promise Error: \n" + err.stack);
+});
+
 bot.on('message', m => {
     if (m.author.bot) return;
-
-    if (checklevel(m) == 0) {
+	
+	
+    if (checklevel(m.member) == 0) {
         if (capsLimit(m)) {
             m.delete();
-            return m.reply("Calme toi sur les majuscules !").then(m => {
+            return m.reply("Calme toi sur les majuscules !")
+                .then(m => {
                 m.delete(3000)
-            }).catch(console.error);
+            })
+                .catch(console.error);
         }
     }
     if (!m.content.startsWith(config.prefix)) return;
     var cmd = m.content.split(' ')[0].substring(config.prefix.length).toLowerCase();
     let args = m.content.split(" ").slice(1).join(" ");
     if (commands.hasOwnProperty(cmd))
-        if (checklevel(m) >= commands[cmd].level) {
+        if (checklevel(m.member) >= commands[cmd].level) {
             commands[cmd].exec(m);
+			if(cmd !== "eval"){
             console.log("---------\n" +
                 "\033[32;1m" + new Date() + "\033[0m\n" +
                 "\033[33;1mCommand:\033[0m\033[33m " + cmd + "\033[0m\n" +
                 "\033[33;1mArguments:\033[0m\033[33m " + args + "\033[0m\n" +
                 "\033[31;1mUtilisateur:\033[0m\033[31m " + m.author.username + "(" + m.author.id + ")\033[0m\n" +
-                "\033[36;1mResult:\033[0m\033[36m " + (checklevel(m) >= commands[cmd].level) + "\033[0m")
+                "\033[36;1mAuthorized:\033[0m\033[36m " + (checklevel(m.member) >= commands[cmd].level) + "\033[0m")
+			}
         } else {
-            m.reply(`Vous n'avez pas la permission d'utiliser cette commande (Votre niveau de permission est trop faible: **${checklevel(m)}** au lieu de **${commands[cmd].level}**)`)
+            m.reply(`Vous n'avez pas la permission d'utiliser cette commande (Votre niveau de permission est trop faible: **${checklevel(m.member)}** au lieu de **${commands[cmd].level}**)`)
         }
 });
 
@@ -66,17 +83,11 @@ var commands = {
                 let embed = new Discord.RichEmbed().setTitle(`Liste des commandes sur le serveur ${message.guild.name}`).setColor("#FF7F50")
                     .addField('Aide pour la syntaxe', '[paramètre optionnel] <paramètre requis>');
                 for (cmd in commands) {
-                    if (commands[cmd].level <= checklevel(message))
+                    if (commands[cmd].level <= checklevel(message.member))
                         embed.addField(`__**${cmd}**__`, "Description: " + commands[cmd].description + "\n"
                             + "Syntaxe: " + commands[cmd].syntax);
                 }
                 message.channel.sendEmbed(embed);
-                //Envoi de message privé a la personne
-
-                /*
-                 message.author.sendEmbed(embed);
-                 message.reply("Regarde tes messages privés pour voir la listes des commandes")
-                 */
 
             }
         },
@@ -85,11 +96,12 @@ var commands = {
             syntax: config.prefix + 'ping',
             level: 0,
             exec: function (message) {
-                message.channel.sendMessage("Ping...").then(mm => {
+                message.channel.sendMessage("Ping...")
+                    .then(mm => {
                     mm.edit(`Pong, took ${mm.createdTimestamp - message.createdTimestamp} ms`)
                 })
             }
-        },
+        },     
         'serverinfo': {
             description: 'Affiche des informations sur le serveur Discord',
             syntax: config.prefix + 'serverinfo',
@@ -115,46 +127,48 @@ var commands = {
             exec: function (message) {
                 let args = message.content.split(" ").slice(1);
                 if (!args[0]) {
+                    let user_level = checklevel(message.member);
                     let embed = new Discord.RichEmbed().setTitle(`Information pour ce membre`).setColor("#51adf6").setThumbnail(message.author.avatarURL)
                     embed.addField("Nom", message.author.username + "#" + message.author.discriminator + " (" + message.author.id + ")")
                     embed.addField("Alias", message.member.nickname)
+					embed.addField("Rejoind le server",message.member.joinedAt)
                     embed.addField("Rôles", message.member.roles.array().splice(1).map(r =>r.name).join(", "))
                     embed.addField("Status", message.member.user.presence.status)
-                    if (checklevel(message) === 3) {
-                        embed.addField("Permission level", checklevel(message) + " |Bot Owner")
-                    } else if (checklevel(message) === 2) {
-                        embed.addField("Permission level", checklevel(message) + " |Administrateur")
-                    } else if (checklevel(message) === 1) {
-                        embed.addField("Permission level", checklevel(message) + " |Modérateur")
-                    } else {
-                        embed.addField("Permission level", checklevel(message) + " |Membre")
-                    }
+                    embed.addField("Permission level",user_level +" | "+ level_alias[user_level])
                     return message.channel.sendEmbed(embed);
                 } else {
                     let guildMember = message.guild.member(message.mentions.users.first());
+                    let user_level =  checklevel(guildMember)
                     if (!guildMember) return message.reply("Utilisateur inconnu.");
                     let embed = new Discord.RichEmbed().setColor("#51adf6").setThumbnail(guildMember.user.avatarURL)
                     embed.setTitle(`Information pour le membre ${guildMember.user.username}`)
                     embed.addField("Nom", guildMember.user.username + "#" + guildMember.user.discriminator + " (" + guildMember.id + ")")
                     embed.addField("Alias", guildMember.nickname)
+					embed.addField("Rejoind le server",guildMember.joinedAt)
                     embed.addField("Rôles", guildMember.roles.array().splice(1).map(r =>r.name).join(", "))
                     embed.addField("Status", guildMember.user.presence.status)
-                    if (checklevelforUser(guildMember) === 3) {
-                        embed.addField("Permission level", checklevelforUser(guildMember) + " |Bot Owner")
-                    } else if (checklevelforUser(guildMember) === 2) {
-                        embed.addField("Permission level", checklevelforUser(guildMember) + " |Administrateur")
-                    } else if (checklevelforUser(guildMember) === 1) {
-                        embed.addField("Permission level", checklevelforUser(guildMember) + " |Modérateur")
-                    } else {
-                        embed.addField("Permission level", checklevelforUser(guildMember) + " |Membre")
-                    }
+                    embed.addField("Permission level",user_level +" | "+ level_alias[user_level])
                     embed.setColor("#51adf6");
                     return message.channel.sendEmbed(embed);
-
                 }
 
             }
         },
+		'version': {
+			description: "Affiche la version du bot discord",
+			syntax: config.prefix + "version",
+			level: 0,
+			exec: function(message){
+				 let embed = new Discord.RichEmbed().setColor("#147006").setTitle("Information Bot discord").setThumbnail(bot.user.avatarURL)
+				 embed.addField("Créateur","Sabrus")
+				 embed.addField("Date de création",bot.user.createdAt)
+				 embed.addField("Date de derniere modification", new Date(2017,02,16))
+				 embed.addField("Version du bot","2.1")
+				 embed.addField("API Discord",`Discord.js v${Discord.version}`)
+				 return message.channel.sendEmbed(embed);
+			}
+			
+		},
         'purge': {
             description: "Supprime tout les messages d'un montant donné",
             syntax: config.prefix + 'purge <nombre 2-100>',
@@ -171,15 +185,22 @@ var commands = {
                     return message.reply("Le bot ne peut pas utiliser cette commande (MANAGE_MESSAGES) est requis")
                 }
                 message.delete()
-                message.channel.fetchMessages({limit: parseInt(args[0])}).then(messages => {
-                    message.channel.bulkDelete(messages).then(m => {
+                message.channel.fetchMessages({limit: parseInt(args[0])})
+                    .then(messages => {
 
-                    }).catch(console.error)
+                    message.channel.bulkDelete(messages)
+                        .then(m => {
 
-                    message.channel.sendMessage(`__**${messages.size}**__ message(s) ont été supprimés !`).then(mm => {
+                    })
+                        .catch(console.error)
+
+                    message.channel.sendMessage(`__**${messages.size}**__ message(s) ont été supprimés !`)
+                        .then(mm => {
                         mm.delete(4000)
                     })
-                }).catch(console.error)
+                })
+                    .catch(console.error)
+
 
             }
         },
@@ -229,11 +250,31 @@ var commands = {
                     annonce = args.splice(1).join(" ");
                 }
                 if (!channel) return message.reply("Le channel ne semble pas valide")
-                channel.sendMessage(`__**ANNONCE**__ \n\n${annonce}`).catch(console.error)
+					const embed = new Discord.RichEmbed().setTitle("__**ANNONCE**__").setColor("#F00")
+						embed.setDescription(annonce)	
+                channel.sendEmbed(embed)
 
             }
-        }
-        ,
+        },
+        'say': {
+            description: 'Le bot ecrit le message que vous lui demander dans un canal donnée',
+            syntax: config.prefix + 'say [#channel] <message>',
+            level: 1,
+            exec: function (message) {
+                let args = message.content.split(" ").slice(1);
+				message.delete();
+                let msg;
+                if (message.mentions.channels.size === 0) {
+                    var channel = message.channel
+                    msg = args.join(" ");
+                } else {
+                    channel = message.mentions.channels.first()
+                    msg = args.splice(1).join(" ");
+                }
+                if (!channel) return message.reply("Le channel ne semble pas valide")
+                channel.sendMessage(`${msg}`).catch(console.error)
+            }
+        },
         'ban': {
             description: 'Ban une personne du serveur',
             syntax: config.prefix + 'ban <@User mention> [delete 0-7 day message]',
@@ -255,15 +296,20 @@ var commands = {
                 if (!banMember) {
                     return message.reply("Cet utilisateur est incorrect ou n'existe pas");
                 }
-
+				
+				if(!banMember.bannable){
+					return message.reply("Cet utilisateur ne peut etre banni");
+				}
+				
                 let args = message.content.split(" ").slice(1);
                 if (isNaN(args[1]) || args[1] > 7) args[1] = 0;
-                banMember.ban(parseInt(args[1])).then(member => {
+                banMember.ban(parseInt(args[1]))
+                    .then(member => {
                     return message.channel.sendMessage(`Le membre **${member.user.username}**(${member.user.id}) à bien été banni`)
-                }).catch(console.error)
+                })
+                    .catch(console.error)
             }
-        }
-        ,
+        },
         'unban': {
             description: 'Débannir une personne du serveur',
             syntax: config.prefix + 'unban <id>',
@@ -286,9 +332,10 @@ var commands = {
 
                 message.guild.fetchBans().then(ban => {
                     if (ban.has(parseInt(args[0]))) {
-                        message.guild.unban(args[0]).then(user =>
-                            message.reply(`Cet utilisateur est maintenant débanni`)
-                        ).catch(console.error);
+                        message.guild.unban(args[0])
+                            .then(user =>
+                            message.reply(`Cet utilisateur est maintenant débanni`))
+                            .catch(console.error);
 
                     } else {
                         return message.reply(`Cet id est inconnu de la banlist`)
@@ -310,9 +357,9 @@ var commands = {
                     var evaled = eval(code);
                     if (typeof evaled !== "string")
                         evaled = require("util").inspect(evaled);
-                    message.channel.sendCode("xl", clean(evaled));
+                    message.channel.sendCode("javascript", clean(evaled));
                 } catch (err) {
-                    message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+                    message.channel.sendMessage(`\`ERROR\` \`\`\`javascript\n${clean(err)}\n\`\`\``);
                 }
             }
         }
@@ -320,36 +367,19 @@ var commands = {
     }
     ;
 
-function checklevel(message) {
-    if (message.author.id === '246779580902277121') {
-        return 3;
-    }
-    if (message.author.id === '265018556133933068') {
-        return 3;
-    }
 
-    if (message.guild.ownerID === message.author.id) {
-        return 2
-    }
-
-    let level =message.guild.member.highestRole.position
-    let AdminRole = message.guild.roles.array().filter(r => r.id === `${config.adminrole}`)[0].position
-    let ModRole = message.guild.roles.array().filter(r => r.id === `${config.modrole}`)[0].position
-    if (level >= AdminRole) {
-        return 2
-    }
-    if (level >= ModRole) {
-        return 1
-    }
-    return 0
-
-
-}
-
-function checklevelforUser(user) {
+function checklevel(user) {
     if (user.id === '246779580902277121') {
         return 3;
+	}
+    if (user.id === '265018556133933068') {
+        return 3;
     }
+
+    if (user.guild.ownerID === user.id) {
+        return 2;
+    }
+
     let level = user.highestRole.position
 
     let AdminRole = user.guild.roles.array().filter(r => r.id === `${config.adminrole}`)[0].position
@@ -362,9 +392,12 @@ function checklevelforUser(user) {
     }
     return 0
 
-
 }
 
+/*
+ * Return true si le nombre de majusucle dépasse le nombre autorisé
+ * Return false sinon
+ * */
 function capsLimit(m) {
     let ww = m.content.split(" ");
     ww.forEach((w, c) => {
@@ -404,6 +437,5 @@ function clean(text) {
     else
         return text;
 }
-
 //Login with TOKEN for Bot
 bot.login(config.token); // Find your token > https://discordapp.com/developers/applications/me
